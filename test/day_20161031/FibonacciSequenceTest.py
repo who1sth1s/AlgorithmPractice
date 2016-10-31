42e93ec4f4ed97d89e86d73c260ec583611bef2e
Who1sth1s <ksjlike69@gmail.com>
2016-11-01 00:41:23 +0900

    FibonacciSequenceTest.py: 피보나치 수열 테스트

---
+++ test/day_20161031/FibonacciSequenceTest.py
@@ -0,0 +1,27 @@
+import unittest
+
+from main.day_20161031.FibonacciSequence import *
+
+class FibonacciSequenceTestClass(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def testGetFibonacciNumberByIterate(self):
+        self.assertEqual(FibonacciSequence().get_fibonacci_number_by_iterate(1), 0)
+        self.assertEqual(FibonacciSequence().get_fibonacci_number_by_iterate(2), 1)
+        self.assertEqual(FibonacciSequence().get_fibonacci_number_by_iterate(3), 1)
+        self.assertEqual(FibonacciSequence().get_fibonacci_number_by_iterate(4), 2)
+        self.assertEqual(FibonacciSequence().get_fibonacci_number_by_iterate(5), 3)
+
+    def testGetFibonacciNumberByRecursive(self):
+        fibonacci_sequence = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+        for index in range(1, 6):
+            fibonacci_sequence_class = FibonacciSequence()
+            fibonacci_sequence_class.get_fibonacci_number_by_recursive(index)
+            self.assertEqual(fibonacci_sequence_class.FIBONACCI_LIST[index-1], fibonacci_sequence[index-1])
+
+    if __name__ == '__main__':
+        unittest.main()
