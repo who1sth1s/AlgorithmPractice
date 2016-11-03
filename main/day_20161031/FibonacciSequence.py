0d61bfbdd61ced6ea1b0602f54662d0a697654d7
Who1sth1s <ksjlike69@gmail.com>
2016-11-04 04:05:40 +0900

    리스트 사용하지 않고 피보나치 구현

--- main/day_20161031/FibonacciSequence.py
+++ main/day_20161031/FibonacciSequence.py
@@ -4,6 +4,8 @@ class FibonacciSequence:
     def __init__(self):
         self.FIRST_NUMBER = 0
         self.SECOND_NUMBER = 1
+        self.FINALLY_NUMBER = 0
+        self.ROOTIN_COUNT = 1
         self.FIBONACCI_LIST = list()
         self.FIBONACCI_LIST.append(self.FIRST_NUMBER)
         self.FIBONACCI_LIST.append(self.SECOND_NUMBER)
@@ -18,8 +20,22 @@ class FibonacciSequence:
     def get_fibonacci_number_by_recursive(self, args_number):
         if args_number > 0:
             args_number -= 1
-            self.FIBONACCI_LIST.append(self.FIBONACCI_LIST[-2] + self.FIBONACCI_LIST[-1])
-            self.get_fibonacci_number_by_recursive(args_number)
+            # self.FIBONACCI_LIST.append(self.FIBONACCI_LIST[-2] + self.FIBONACCI_LIST[-1])
+            if self.ROOTIN_COUNT == 1:
+                self.ROOTIN_COUNT += 1
+                self.FINALLY_NUMBER = self.FIRST_NUMBER
+                self.get_fibonacci_number_by_recursive(args_number)
+
+            elif self.ROOTIN_COUNT == 2:
+                self.ROOTIN_COUNT += 1
+                self.FINALLY_NUMBER = self.SECOND_NUMBER
+                self.get_fibonacci_number_by_recursive(args_number)
+
+            else:
+                self.FINALLY_NUMBER = self.FIRST_NUMBER + self.SECOND_NUMBER
+                self.FIRST_NUMBER = self.SECOND_NUMBER
+                self.SECOND_NUMBER = self.FINALLY_NUMBER
+                self.get_fibonacci_number_by_recursive(args_number)
 
 '''
 start = time.time()
