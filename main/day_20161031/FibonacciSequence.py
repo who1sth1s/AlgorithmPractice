3bb29f5eff793463391d573d6556ccdbd56415c6
Who1sth1s <ksjlike69@gmail.com>
2016-11-06 01:48:08 +0900

    class 제거

--- main/day_20161031/FibonacciSequence.py
+++ main/day_20161031/FibonacciSequence.py
@@ -1,25 +1,49 @@
-def get_fibonacci_number_by_iterate(self, args_number):
-    if args_number > 2:
-        for index in range(2, args_number):
-            self.FIBONACCI_LIST.append(self.FIBONACCI_LIST[index-2] + self.FIBONACCI_LIST[index-1])
-    return self.FIBONACCI_LIST[args_number-1]
+import sys
 
 
-def get_fibonacci_number_by_recursive(self, args_number):
+sys.setrecursionlimit(1500)
+
+
+def get_fibonacci_number_by_iterate(args_number):
+    first_number = 0
+    second_number = 1
+    finally_number = 0
+
+    if args_number == 1:
+        finally_number = first_number
+
+    elif args_number == 2:
+        finally_number = second_number
+
+    else:
+        for index in range(int(args_number) - 2):
+            finally_number = first_number + second_number
+            first_number = second_number
+            second_number = finally_number
+
+    return finally_number
+
+FINALLY_NUMBER = 0
+FIRST_NUMBER = 0
+SECOND_NUMBER = 1
+
+
+def get_fibonacci_number_by_recursive(args_number):
+    global FINALLY_NUMBER, FIRST_NUMBER, SECOND_NUMBER
+
     if args_number > 0:
         args_number -= 1
-        if self.ROOTIN_COUNT == 1:
-            self.ROOTIN_COUNT += 1
-            self.FINALLY_NUMBER = self.FIRST_NUMBER
-            self.get_fibonacci_number_by_recursive(args_number)
+        if args_number == 0:
+            FINALLY_NUMBER = FIRST_NUMBER
 
-        elif self.ROOTIN_COUNT == 2:
-            self.ROOTIN_COUNT += 1
-            self.FINALLY_NUMBER = self.SECOND_NUMBER
-            self.get_fibonacci_number_by_recursive(args_number)
+        elif args_number == 1:
+            FINALLY_NUMBER = SECOND_NUMBER
 
-        else:
-            self.FINALLY_NUMBER = self.FIRST_NUMBER + self.SECOND_NUMBER
-            self.FIRST_NUMBER = self.SECOND_NUMBER
-            self.SECOND_NUMBER = self.FINALLY_NUMBER
-            self.get_fibonacci_number_by_recursive(args_number)
+        elif args_number > 1:
+            FINALLY_NUMBER = FIRST_NUMBER + SECOND_NUMBER
+            FIRST_NUMBER = SECOND_NUMBER
+            SECOND_NUMBER = FINALLY_NUMBER
+            get_fibonacci_number_by_recursive(args_number)
+
+    finally_number = FINALLY_NUMBER
+    return finally_number
