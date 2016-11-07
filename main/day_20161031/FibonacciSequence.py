9ac1e77e21625a50ca57b3f26fc881abdcb4d4bc
Who1sth1s <ksjlike69@gmail.com>
2016-11-08 00:12:34 +0900

    로직 수정

--- main/day_20161031/FibonacciSequence.py
+++ main/day_20161031/FibonacciSequence.py
@@ -24,26 +24,24 @@ def get_fibonacci_number_by_iterate(args_number):
     return finally_number
 
 FINALLY_NUMBER = 0
-FIRST_NUMBER = 0
-SECOND_NUMBER = 1
+FIRST_SECOND_DICT = {
+    1: 0,
+    2: 1
+}
 
 
 def get_fibonacci_number_by_recursive(args_number):
-    global FINALLY_NUMBER, FIRST_NUMBER, SECOND_NUMBER
+    global FINALLY_NUMBER
 
-    if args_number > 0:
+    if args_number in FIRST_SECOND_DICT:
+        FINALLY_NUMBER = FIRST_SECOND_DICT.get(args_number)
+
+    else:
         args_number -= 1
-        if args_number == 0:
-            FINALLY_NUMBER = FIRST_NUMBER
-
-        elif args_number == 1:
-            FINALLY_NUMBER = SECOND_NUMBER
-
-        elif args_number > 1:
-            FINALLY_NUMBER = FIRST_NUMBER + SECOND_NUMBER
-            FIRST_NUMBER = SECOND_NUMBER
-            SECOND_NUMBER = FINALLY_NUMBER
-            get_fibonacci_number_by_recursive(args_number)
+        FINALLY_NUMBER = FIRST_SECOND_DICT.get(1) + FIRST_SECOND_DICT.get(2)
+        FIRST_SECOND_DICT[1] = FIRST_SECOND_DICT[2]
+        FIRST_SECOND_DICT[2] = FINALLY_NUMBER
+        get_fibonacci_number_by_recursive(args_number)
 
     finally_number = FINALLY_NUMBER
     return finally_number
