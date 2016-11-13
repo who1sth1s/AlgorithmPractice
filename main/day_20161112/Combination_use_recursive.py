acf7a139d21fdfc58098f47615342557ae1e8f92
Who1sth1s <ksjlike69@gmail.com>
2016-11-14 00:23:41 +0900

    1, 2, 3으로 숫자 계산 재귀 사용

--- main/day_20161112/Combination_use_recursive.py
+++ main/day_20161112/Combination_use_recursive.py
@@ -1,2 +1,15 @@
+COMBINATION_COUNT = 0
+
+
 def combination_recursive(args):
-    return 1
+    if args == 0:
+        global COMBINATION_COUNT
+        COMBINATION_COUNT += 1
+    if args - 1 >= 0:
+        combination_recursive(args-1)
+    if args - 2 >= 0:
+        combination_recursive(args-2)
+    if args - 3 >= 0:
+        combination_recursive(args-3)
+
+    return COMBINATION_COUNT
